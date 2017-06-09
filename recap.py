8164577e30fcb7829f829dae68f94b6fe9d50294
allblackkankam <kankamsolomon@gmail.com>
2017-06-09 18:08:37 +0100

    recaping

---
+++ recap.py
@@ -0,0 +1,30 @@
+#Numbers
+print 1+1	
+
+#List-
+List = ["boys","girls","men"]
+print List
+
+String
+String = "what is your name"
+print String[12:17]
+
+Dictionary
+cars = {"VW":"VW","GM":"FORD","VW":"AUDI"}
+print cars["GM"]
+
+
+#Tuples 
+Drinks =("coke","fanta")
+print Drinks[0]
+
+goods=raw_input("what is the no. of PC") 
+if goods=="100":
+	print goods, "Everything is OK"
+
+elif goods=="80":
+	print goods, "Goods not upto"
+
+else:
+	print "NO"
+
