31680e76bf49afd80ee43c250266947680b8dd55
allblackkankam <kankamsolomon@gmail.com>
2017-06-10 11:48:41 +0100

    recaping 2

---
+++ recapONloops.py
@@ -0,0 +1,43 @@
+# x = ["mon","tue","wed","thus","fri","sat","sun"]
+# for days in x:		
+# 	if days=="wed":
+# 		continue
+# 		print "A Great Day"
+# 	print days
+
+# x = ["mon","tue","wed","thus","fri","sat","sun"]
+# for days in x:		
+# 	if days =="wed":
+# 		pass
+# 		print "passing day"
+# 	print days
+
+# x = ["mon","tue","wed","thus","fri","sat","sun"]
+# for days in x:		
+# 	if days=="wed":
+# 		break
+# 		print "A Great Day"
+# 	print days
+	
+
+# x =100
+# while x<110:
+# 	x= x+1
+# 	if x==105:
+# 		break
+# 	print x
+
+# x =100
+# while x<110:
+# 	x= x+1
+# 	if x==105:
+# 		continue
+# 	print x
+	
+x =100
+while x<110:
+	x= x+1
+	if x==105:
+		pass
+		print "passing"
+	print x
