625e398bb6c471f057a8b17f53aea5599c3a98ee
allblackkankam <kankamsolomon@gmail.com>
2017-06-10 11:52:20 +0100

    codes of functions

---
+++ functions.py
@@ -0,0 +1,25 @@
+# def x(one):
+# 	 print one+one 
+
+# x(4)
+
+
+def form():
+	name = raw_input("what is your name?")
+	age = raw_input("How old are you?")
+	gender = raw_input("what is your gender?")
+	address = raw_input("where do you live?")
+	return name , age,gender,address
+
+print form()
+
+def x(one,two):
+	 print one+two
+
+x(4,8)
+
+# def x(one):
+# 	 print one+one 
+
+# x(4)
+
