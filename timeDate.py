fd35fbf15aef226e7de00986d22d84d855b9c34a
allblackkankam <kankamsolomon@gmail.com>
2017-06-10 15:41:31 +0100

    time and dates

---
+++ timeDate.py
@@ -0,0 +1,13 @@
+# import time
+# ticks = time.time()
+# print "Number of ticks since 12:00am,January 1,2000:",ticks
+# print "Number of ticks since 12:00am,June 10,2017:",ticks
+
+# import time
+# localtime=time.asctime(time.localtime(time.time()))
+# print "Local current time:",localtime
+
+import calendar
+cal = calendar.month(2017,1)
+print "Here is the calendar:"
+print cal
